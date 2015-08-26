4aaa048fa6a8ac4f313ae02c52e5e87ec25c76cf
memoxmrdl <jmemox@gmail.com>
2015-08-26 11:41:36 -0500

    Removed view_path for lazy_scope of ActionView module

--- lib/i18n_lazy_scope/action_view/helper.rb
+++ lib/i18n_lazy_scope/action_view/helper.rb
@@ -1,17 +1,7 @@
 module I18nLazyScope::ActionView
   module Helper
     def lazy_scope
-      [*I18nLazyScope.configuration.action_view_scope, view_path]
-    end
-
-  private
-
-    def view_path
-      if @virtual_path
-        @virtual_path.gsub(%r{/_?}, ".")
-      else
-        raise "Cannot use lazy lookup because template path is not available"
-      end
+      [*I18nLazyScope.configuration.action_view_scope]
     end
   end
 end
